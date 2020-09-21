8385b2b490afb4beea6f4fd73fdbfef55e76905f
Dave Barragan <itsdaveba@gmail.com>
2020-09-21 09:47:07 -0500

    test format

--- solver/defs.py
+++ solver/defs.py
@@ -22,30 +22,31 @@ class Move(IntEnum):
 	B3 = 17
 
 
-keys_values = [[(Move.U1, Move.U2, Move.U3),
-				(Move.R1, Move.R2, Move.R3, Move.F1, Move.F2, Move.F3,
-				 Move.D1, Move.D2, Move.D3, Move.L1, Move.L2, Move.L3,
-				 Move.B1, Move.B2, Move.B3)],
-			   [(Move.R1, Move.R2, Move.R3),
-				(Move.U1, Move.U2, Move.U3, Move.F1, Move.F2, Move.F3,
-				 Move.D1, Move.D2, Move.D3, Move.L1, Move.L2, Move.L3,
-				 Move.B1, Move.B2, Move.B3)],
-			   [(Move.F1, Move.F2, Move.F3),
-				(Move.U1, Move.U2, Move.U3, Move.R1, Move.R2, Move.R3,
-				 Move.D1, Move.D2, Move.D3, Move.L1, Move.L2, Move.L3,
-				 Move.B1, Move.B2, Move.B3)],
-			   [(Move.D1, Move.D2, Move.D3),
-				(Move.R1, Move.R2, Move.R3, Move.F1, Move.F2, Move.F3,
-				 Move.L1, Move.L2, Move.L3, Move.B1, Move.B2, Move.B3)],
-			   [(Move.L1, Move.L2, Move.L3),
-				(Move.U1, Move.U2, Move.U3, Move.F1, Move.F2, Move.F3,
-				 Move.D1, Move.D2, Move.D3, Move.B1, Move.B2, Move.B3)],
-			   [(Move.B1, Move.B2, Move.B3),
-				(Move.U1, Move.U2, Move.U3, Move.R1, Move.R2, Move.R3,
-				 Move.D1, Move.D2, Move.D3, Move.L1, Move.L2, Move.L3)]]
+keys_values = [
+[(Move.U1, Move.U2, Move.U3),
+(Move.R1, Move.R2, Move.R3, Move.F1, Move.F2, Move.F3,
+	Move.D1, Move.D2, Move.D3, Move.L1, Move.L2, Move.L3,
+	Move.B1, Move.B2, Move.B3)],
+[(Move.R1, Move.R2, Move.R3),
+(Move.U1, Move.U2, Move.U3, Move.F1, Move.F2, Move.F3,
+	Move.D1, Move.D2, Move.D3, Move.L1, Move.L2, Move.L3,
+	Move.B1, Move.B2, Move.B3)],
+[(Move.F1, Move.F2, Move.F3),
+(Move.U1, Move.U2, Move.U3, Move.R1, Move.R2, Move.R3,
+	Move.D1, Move.D2, Move.D3, Move.L1, Move.L2, Move.L3,
+	Move.B1, Move.B2, Move.B3)],
+[(Move.D1, Move.D2, Move.D3),
+(Move.R1, Move.R2, Move.R3, Move.F1, Move.F2, Move.F3,
+	Move.L1, Move.L2, Move.L3, Move.B1, Move.B2, Move.B3)],
+[(Move.L1, Move.L2, Move.L3),
+(Move.U1, Move.U2, Move.U3, Move.F1, Move.F2, Move.F3,
+	Move.D1, Move.D2, Move.D3, Move.B1, Move.B2, Move.B3)],
+[(Move.B1, Move.B2, Move.B3),
+(Move.U1, Move.U2, Move.U3, Move.R1, Move.R2, Move.R3,
+	Move.D1, Move.D2, Move.D3, Move.L1, Move.L2, Move.L3)]]
 
 available_moves = {key: value for keys, value in keys_values for key in keys}
 available_moves.update({None:
-					   (Move.U1, Move.U2, Move.U3, Move.R1, Move.R2, Move.R3,
-				 		Move.F1, Move.F2, Move.F3, Move.D1, Move.D2, Move.D3,
-				 		Move.L1, Move.L2, Move.L3, Move.B1, Move.B2, Move.B3)})
+	(Move.U1, Move.U2, Move.U3, Move.R1, Move.R2, Move.R3,
+		Move.F1, Move.F2, Move.F3, Move.D1, Move.D2, Move.D3,
+		Move.L1, Move.L2, Move.L3, Move.B1, Move.B2, Move.B3)})
